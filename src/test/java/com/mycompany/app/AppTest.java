4129c273a073d8398174752b0d1a84637b82a0e3
maharshi18 <40627974+maharshi18@users.noreply.github.com>
2020-05-03 02:54:31 +0530

    trying with old calc

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -1,20 +1,80 @@
 package com.mycompany.app;
 
-import static org.junit.Assert.assertTrue;
 
-import org.junit.Test;
+import org.junit.*;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class AppTest {
+    
+    private App calculator;
+    
+    @Before
+    public void setUp() {
+        calculator = new App();
+    }
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void testAdd() {
+        int a = 15;
+        int b = 20;
+        int expectedResult = 35;
+        long result = calculator.add(a, b);
+        Assert.assertEquals(expectedResult, result);;
+    }
+    
+    @Test
+    public void testSubtract() {
+        int a = 25;
+        int b = 20;
+        int expectedResult = 5;
+        long result = calculator.subtract(a, b);
+        Assert.assertEquals(expectedResult, result);;
+    }
+    
+    @Test
+    public void testMultiply() {
+        int a = 10;
+        int b = 25;
+        long expectedResult = 250;
+        long result = calculator.multiply(a, b);
+        Assert.assertEquals(expectedResult, result);;
+    }
+    
+    @Test
+    public void testDivide() {
+        int a = 56;
+        int b = 10;
+        double expectedResult = 5.6;
+        double result = calculator.divide(a, b);
+        Assert.assertEquals(expectedResult, result,0.00005);
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void testDivideByZero() {
+        int a = 15;
+        int b = 0;
+        calculator.divide(a, b);
     }
 }
+
+
+
+
+
+// import static org.junit.Assert.assertTrue;
+
+// import org.junit.Test;
+
+// /**
+//  * Unit test for simple App.
+//  */
+// public class AppTest 
+// {
+//     /**
+//      * Rigorous Test :-)
+//      */
+//     @Test
+//     public void shouldAnswerWithTrue()
+//     {
+//         assertTrue( true );
+//     }
+// }
